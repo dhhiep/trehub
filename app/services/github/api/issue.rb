3c005c1242a651600e6c377d3bcb37a1aff50d46
Hiep Dinh <hiep.dinh@fooder.io>
2021-09-04 16:51:57 +0700

    Issue_2: Add service Github::Api::Issue
    https://github.com/dhhiep/trehub/issues/2

---
+++ app/services/github/api/issue.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Github
+  module Api
+    class Issue < Base
+      # List issues for a repository
+      #
+      # @param options [Sawyer::Resource] A customizable set of options.
+      # @option options [Integer] :milestone Milestone number.
+      # @option options [String] :state (open) State: <tt>open</tt>, <tt>closed</tt>, or <tt>all</tt>.
+      # @option options [String] :assignee User login.
+      # @option options [String] :creator User login.
+      # @option options [String] :mentioned User login.
+      # @option options [String] :labels List of comma separated Label names. Example: <tt>bug,ui,@high</tt>.
+      # @option options [String] :sort (created) Sort: <tt>created</tt>, <tt>updated</tt>, or <tt>comments</tt>.
+      # @option options [String] :direction (desc) Direction: <tt>asc</tt> or <tt>desc</tt>.
+      # @option options [Integer] :page (1) Page number.
+      # @return [Array<Sawyer::Resource>] A list of issues for a repository.
+      # @see https://developer.github.com/v3/issues/#list-issues-for-a-repository
+      # @see https://developer.github.com/v3/issues/#list-issues
+      # @example List issues
+      #   issues(page: 2, per_page: 50)
+      def issues(options = {})
+        @issues ||= client.issues(repo, options)
+
+        formatter(@issues)
+      end
+
+      private
+
+      def formatter(data)
+        data.reduce({}) do |accumulator, issue|
+          issue_data = {
+            number: issue[:number],
+            name: issue[:title],
+            status: issue[:state],
+            assignees: assignees(issue),
+            labels: labels(issue),
+            milestone: milestone(issue),
+          }
+
+          accumulator.merge(issue[:number] => issue_data)
+        end
+      end
+
+      def assignees(issue)
+        issue.assignees.map do |assignee|
+          {
+            name: assignee[:login],
+            avatar: assignee[:avatar_url],
+          }
+        end
+      end
+
+      def labels(issue)
+        issue.labels.map do |label|
+          {
+            name: label[:name],
+            color: label[:color],
+          }
+        end
+      end
+
+      def milestone(issue)
+        due_on = issue.dig(:milestone, :due_on)
+        return due_on if due_on.blank?
+
+        Time.parse(due_on)
+      end
+    end
+  end
+end
