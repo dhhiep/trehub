05acb674967ed973948663e05bf8ae6acb14d32b
Hiep Dinh <hoanghiepitvnn@gmail.com>
2022-02-16 15:27:40 +0700

    Refactor assets

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,14 +1,9 @@
-import $ from 'jquery'
-import 'select2'
-import 'select2/dist/css/select2.css'
+import $ from "jquery";
+import "select2";
+import "select2/dist/css/select2.css";
 
-$(function() {
-  $('.select2').select2({
-    width: '100%',
-  });
-
-  $(".btn-click").on("click", function() {
-    url = $(this).attr("data-url")
-    window.location.href = (url);
+$(function () {
+  $(".select2").select2({
+    width: "100%",
   });
 });
